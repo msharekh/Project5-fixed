853ebf41ec3f04753d47d921a1a1f4f5d3644da0
Mishal Alsharekh <mishal.sharekh@gmail.com>
2019-01-21 12:43:43 +0300

    working web

--- truffle.js
+++ truffle.js
@@ -10,6 +10,11 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*' // Match any network id
+    },
+    development: {
+      host: '127.0.0.1',
+      port: 9545,
+      network_id: '*' // Match any network id
     }
   }
 }
